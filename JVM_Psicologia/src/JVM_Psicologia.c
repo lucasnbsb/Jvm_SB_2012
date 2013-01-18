ed568f43e68ec6a6cd94aa762de7baa44b98097f
VitorCBSB <vitorcoimbra6@hotmail.com>
2013-01-18 11:32:47 -0200

    Esqueci de remover o TODO, retirado. Mals.

--- JVM_Psicologia/src/JVM_Psicologia.c
+++ JVM_Psicologia/src/JVM_Psicologia.c
@@ -17,7 +17,7 @@ int main(char* argv[], int argc){
 		printf("ERRO! Possíveis causas:\n");
 		printf("\t1. Mais de um argumento de linha de comando foi passado\n");
 		printf("\t2. Nao foi passado um argumento de linha de comando\n");
-		printf("\t3. O arquivo nao possui a extensao .class\n");	//TODO - verificar extensao
+		printf("\t3. O arquivo nao possui a extensao .class\n");
 	}
 
 	return 0;
