170ec9a246f6321214376c6f2fcf9babfab54e85
VitorCBSB <vitorcoimbra6@hotmail.com>
2013-01-18 12:13:22 -0200

    Enviando release (testando no Eclipse)

---
+++ JVM_Psicologia/Release/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/JVM_Psicologia.c 
+
+OBJS += \
+./src/JVM_Psicologia.o 
+
+C_DEPS += \
+./src/JVM_Psicologia.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cygwin C Compiler'
+	gcc -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
