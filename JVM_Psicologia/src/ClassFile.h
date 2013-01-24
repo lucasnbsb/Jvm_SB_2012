5a4c38652a2293c406680cd76b2a6c43bb2bc6a2
VitorCBSB <vitorcoimbra6@hotmail.com>
2013-01-24 09:02:29 -0200

    Criei o arquivo de estrutura do ClassFile. Incompleto ainda, mas o código ainda compila.

---
+++ JVM_Psicologia/src/ClassFile.h
@@ -0,0 +1,48 @@
+/*
+ * ClassFile.h
+ *
+ *  Created on: 24/01/2013
+ *      Author: Vitor
+ */
+
+#ifndef CLASSFILE_H_
+#define CLASSFILE_H_
+
+/*
+ * Definição dos tamanhos de bytes lidos do arquivo .class.
+ */
+typedef unsigned char u1;
+typedef unsigned short u2;
+typedef unsigned int u4;
+typedef unsigned long u8;
+
+typedef union _tipoConstPool{
+
+
+
+} tipoConstPool;
+
+typedef struct _cpInfo{
+
+	u1 tag;
+	tipoConstPool constante;
+
+} cpInfo;
+
+typedef struct _classFile{
+
+	u4 magic;
+
+	u2 minorVersion;
+	u2 majorVersion;
+
+	u2 constantPoolCount;
+
+	/*IMPORTANTE:
+	 * a tabela de constantes é indexada a partir de 1
+	 */
+	cpInfo *constantPool;
+
+} classFile;
+
+#endif /* CLASSFILE_H_ */
