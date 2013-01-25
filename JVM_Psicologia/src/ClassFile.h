2675bb3cf7f21cddb1a8a0839625970e9c9188ca
VitorCBSB <vitorcoimbra6@hotmail.com>
2013-01-25 12:00:22 -0200

    Construindo a tabela de flags de acesso de campos.

--- JVM_Psicologia/src/ClassFile.h
+++ JVM_Psicologia/src/ClassFile.h
@@ -19,7 +19,7 @@ typedef unsigned short u2;
 typedef unsigned int u4;
 typedef unsigned long u8;
 
-//tabela dos access flags
+//tabela dos access flags de classes
 #define ACC_PUBLIC	0x0001	//Declared public; may be accessed from outside its package.
 #define ACC_FINAL	0x0010	//Declared final; no subclasses allowed.
 #define ACC_SUPER	0x0020	//Treat superclass methods specially when invoked by the invokespecial instruction.
@@ -29,7 +29,10 @@ typedef unsigned long u8;
 #define ACC_ANNOTATION	 0x2000 //	Declared as an annotation type.
 #define ACC_ENUM	0x4000	//Declared as an enum type.
 
-
+//Tabela dos access flags de campos (flags não definidas são aproveitadas da tabela de classes)
+#define ACC_PRIVATE		0x0002	//Declared private;
+#define ACC_PROTECTED	0x0004	//Declared protected;
+#define ACC_STATIC		0x0008	//Declared static;
 
 /*
  * Union que indica os vários tipos possíveis da pool de constantes
@@ -101,9 +104,25 @@ typedef union _tipoConstPool{
 
 } tipoConstPool;
 
+typedef struct CP_info {
+    u1 tag; //TODO - tabela de defines pra tags da constant pool
+    tipoConstPool info;
+}cpInfo;
+
 /*
  *
  */
+typedef struct _fieldInfo {
+	u2 accessFlags;
+	u2 nameIndex;
+	u2 descriptorIndex;
+	u2 attributesCount;
+	//attributeInfo *attributes;
+} fieldInfo;
+
+typedef struct _attributeInfo {
+
+};
 
 /*
 typedef struct _cpInfo{
@@ -113,24 +132,21 @@ typedef struct _cpInfo{
 
 // FONTE - http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-4.html
 
-typedef struct CP_info {
-    u1 tag; //TODO - tabela de defines pra tags da constant pool
-    u1 info[];
-}cp_info;
+
 
 typedef struct classfile {
     u4             	magic; //  0xCAFEBABE
     u2             	minor_version; // Versionamento  no formato M.m
     u2             	major_version; // onde M = major version e m=minor version , determina se o arquivo será lido ou não
     u2             	constant_pool_count; // numero de entradas na pool de constantes + 1 , um indice da pool só é valido se estiver entre 0 e constant_pool_count
-    cp_info    		*constant_pool; //deve ser alocado com constant_pool[constant_pool_count-1];
+    cpInfo    		*constant_pool; //deve ser alocado com constant_pool[constant_pool_count-1];
     u2             	access_flags; // tabela de flags a ser consultada para checar a permissão de acesso ao classfile em questão , item 4.1 na fonte //TODO - implementar os defines da tabela
     u2             	this_class; // deve ser uma entrada válida da constant_pool contendo uma estrutura do tipo CONSTANT_Class_info , item 4.4.1 na fonte
     u2             	super_class; // deve ser 0 caso a classe não tenha herdado de uma superclasse ou uma entrada válida na constant pool  com uma struct CONSTANT_Class_info contendo informaçoes sobre a superclasse direta da classe em questão
     u2             	interfaces_count; // contem o número de superinterfaces diretas da classe ou interface em questão
     u2             	*interfaces; // deve ser alocado com interfaces[interfaces_count]  , deve ser uma entrada válida na constant_pool com as informações das interfaces da classe
     u2             	fields_count; // numero de estruturas field_info na tabela de fields
-    //field_info    	*fields; //deve ser alocado com fields[fields_count] ,  cada valor deve ser um field_info(4.5 na fonte) com a descição completa de um field na classe em questão TODO implementar field_info
+    fieldInfo    	*fields; //deve ser alocado com fields[fields_count] ,  cada valor deve ser um field_info(4.5 na fonte) com a descição completa de um field na classe em questão TODO implementar field_info
     u2             	methods_count; // contem o número de estruturas method_info na biblioteca de métodos
     //method_info   *methods; // deve ser alocado com  methods[methods_count] , biblioteca de métodos TODO - implementar method_info
     u2             	attributes_count; // numero de entradas na tabela de atributos
