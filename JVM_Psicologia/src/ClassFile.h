02e167fa874b4ba737fd63443b1a327adca0c4a7
Lucas_Neto <Lucas_Neto@Lucas_Neto-PC>
2013-01-25 11:22:19 -0200

    testando a resolucao de conflitos

--- JVM_Psicologia/src/ClassFile.h
+++ JVM_Psicologia/src/ClassFile.h
@@ -19,8 +19,7 @@ typedef unsigned short u2;
 typedef unsigned int u4;
 typedef unsigned long u8;
 
-
-// Tabela de access_flags válidas
+//tabela dos access flags
 #define ACC_PUBLIC	0x0001	//Declared public; may be accessed from outside its package.
 #define ACC_FINAL	0x0010	//Declared final; no subclasses allowed.
 #define ACC_SUPER	0x0020	//Treat superclass methods specially when invoked by the invokespecial instruction.
